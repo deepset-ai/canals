f7f9b1a584d6a1b4204c4a085c0619d0293f3213
Massimiliano Pippi <mpippi@gmail.com>
2023-09-02 19:38:50 +0200

    thank you mypy protector of the dumb programmers

--- canals/serialization.py
+++ canals/serialization.py
@@ -23,7 +23,7 @@ def component_to_dict(obj: Any) -> Dict[str, Any]:
             param_value = getattr(obj, name)
         except AttributeError as e:
             # If the parameter doesn't have a default value, raise an error
-            if param.empty:
+            if param.default is param.empty:
                 raise SerializationError(
                     f"Cannot determined the value of the init parameter '{name}'. "
                     f"You can fix this error by assigning 'self.{name} = {name}' or adding a "
