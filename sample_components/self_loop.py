81fc4bba96ae097a08dd06c1ec5d8c4b140d8dcf
ZanSara <sara.zanzottera@deepset.ai>
2023-10-24 11:14:38 +0200

    add self-loop integration tests

---
+++ sample_components/self_loop.py
@@ -0,0 +1,30 @@
+from typing import Optional
+
+from canals import component
+
+
+@component
+class SelfLoop:
+    """
+    Decreases the initial value in steps of 1 until the target value is reached.
+
+    For no good reason it uses a self-loop to do so :)
+    """
+
+    def __init__(self, target: int = 0):
+        self.target = target
+
+    @component.output_types(current_value=int, final_result=int)
+    def run(self, initial_value: Optional[int] = None, current_value: Optional[int] = None):
+        """
+        Decreases the initial value in steps of 1 until the target value is reached.
+        """
+        if initial_value:
+            current_value = initial_value
+
+        if current_value:
+            current_value -= 1
+
+        if current_value == self.target:
+            return {"final_result": current_value}
+        return {"current_value": current_value}
